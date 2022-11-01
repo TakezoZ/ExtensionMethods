cf40f4e9156e1bde4b8c3b62da14ec6cf39543fb
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-11-01 11:16:14 -0300

    ExthensionMethod Demo1

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 11, 01, 09, 11, 00);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
